25cc2876f55b743a3b12f12520c8a52af233f3a3
bhaskarAntony <rooparoopa7105@gmail.com>
2025-04-29 19:19:21 +0530

    all files

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar: React.FC<NavbarProps> = ({ scrolled, toggleSidebar }) => {
   return (
     <header 
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-        scrolled ? 'bg-white shadow-md py-2' : 'bg-transparent py-4'
+        scrolled ? 'bg-white shadow-md py-2' : 'bg-transparent py-4 text-white'
       }`}
     >
       <div className="container-custom">
